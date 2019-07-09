724047764505e41d9da6539101ec24f4071ac724
noor <noor.web.journey@gmail.com>
2019-07-09 08:35:44 +0600

    AdomxAdmin Dashboard Templeteing Done

---
+++ resources/views/layouts/backend/_footer.blade.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+
+    <div class="footer-copyright text-center">
+        <p class="text-body-light">2019 &copy; <a href="https://themeforest.net/user/codecarnival">Codecarnival</a></p>
+    </div>
+
+</div>
---
+++ resources/views/layouts/backend/master.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    @include('layouts.backend._head')
+</head>
+
+<body class="skin-dark">
+
+<div class="main-wrapper">
+
+
+    <!-- Header Section Start -->
+    <div class="header-section">
+        @include('layouts.backend._header')
+    </div>
+    <!-- Header Section End -->
+    <!-- Side Header Start -->
+    <div class="side-header show">
+       @include('layouts.backend._mainNave')
+    </div>
+    <!-- Side Header End -->
+
+    <!-- Content Body Start -->
+    <div class="content-body">
+        @yield('content-body')
+        </div>
+    <!-- Content Body End -->
+
+    <!-- Footer Section Start -->
+    <div class="footer-section">
+        @include('layouts.backend._footer')
+    </div><!-- Footer Section End -->
+
+</div>
+
+<!-- JS
+============================================ -->
+
+<!-- Global Vendor, plugins & Activation JS -->
+@include('layouts.backend._scripts')
+
+</body>
+
+</html>
